6bb6fffafbdfc59e85521795fa51327cb3c8beba
ANSHUL <anshuldhiman0106@gmail.com>
2025-05-16 08:07:26 +0530

    Update style.js

--- style.js
+++ style.js
@@ -6,16 +6,16 @@ function showSkeletonLoader() {
   document.getElementById('output').innerHTML = `
     <div class="animate-pulse space-y-4">
       <div class="flex items-center space-x-4">
-        <div class="w-16 h-16 bg-gray-700 rounded-full"></div>
+        <div class="w-16 h-16 bg-gray-400 rounded-full"></div>
         <div class="flex-1 space-y-2">
-          <div class="h-4 bg-gray-700 rounded w-3/4"></div>
-          <div class="h-4 bg-gray-700 rounded w-1/2"></div>
+          <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+          <div class="h-4 bg-gray-400 rounded w-1/2"></div>
         </div>
       </div>
       <div class="space-y-2">
-        <div class="h-4 bg-gray-700 rounded w-1/2"></div>
-        <div class="h-4 bg-gray-700 rounded w-1/3"></div>
-        <div class="h-4 bg-gray-700 rounded w-1/4"></div>
+        <div class="h-4 bg-gray-400 rounded w-1/2"></div>
+        <div class="h-4 bg-gray-400 rounded w-1/3"></div>
+        <div class="h-4 bg-gray-400 rounded w-1/4"></div>
       </div>
     </div>
   `;
