00b53ca03b8ede89d732845c28a13eefc775ff89
anshuldhiman0106 <anshuldhiman0106@gmail.com>
2025-06-12 11:44:32 +0530

    d

--- style.js
+++ style.js
@@ -1,5 +1,6 @@
 // ✅ Replace with your actual API key
-const API_KEY = 'AIzaSyDcEuae5NLQ84-_QC1vtWKjKZybz9VkEJw';
+const API_KEY = window.YT_API_KEY; 
+
 
 // Skeleton Loader
 function showSkeletonLoader() {
